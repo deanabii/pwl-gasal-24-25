21fdb9bb0f80e53073b83548b2ae5e9e1eee53cd
deanabii <117475244+deanabii@users.noreply.github.com>
2024-11-18 15:18:15 +0700

    css

--- react-client/src/components/Contact.jsx
+++ react-client/src/components/Contact.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // import { useNavigate } from "react-router-dom";
 
 // function Contact(){
@@ -17,7 +17,37 @@
 
 // export default Contact;
 
-function Contact() {
+// function Contact() {
+//     const inlineStyle = {
+//         backgroundColor: 'lightblue',
+//         color: "red",
+//         padding: "10px",
+//     }
+//     return (
+//         <div className="container">
+//             <h1 style={inlineStyle}>This is the Contact page</h1>
+//             <button>Go Back</button>
+// =======
+// import { useNavigate } from "react-router-dom";
+
+// function Contact(){
+//     const navigate = useNavigate();
+//     const goBack = () => {
+//         // navigate(-1); // Navigate to the previous page using the "goBack" method from react-router-dom
+//         navigate("/");
+//     };
+//     return(
+//         <div>
+//             <h1>This is the Contact</h1>
+//             <button onClick={goBack}>Go Back</button>
+// >>>>>>> b52313201a88c0084679676a5a4a9b6765985b7b
+//         </div>
+//     );
+// }
+
+// export default Contact;
+
+function Contact(){
     const inlineStyle = {
         backgroundColor: 'lightblue',
         color: "red",
@@ -25,22 +55,7 @@ function Contact() {
     }
     return (
         <div className="container">
-            <h1 style={inlineStyle}>This is the Contact page</h1>
-            <button>Go Back</button>
-=======
-import { useNavigate } from "react-router-dom";
-
-function Contact(){
-    const navigate = useNavigate();
-    const goBack = () => {
-        // navigate(-1); // Navigate to the previous page using the "goBack" method from react-router-dom
-        navigate("/");
-    };
-    return(
-        <div>
-            <h1>This is the Contact</h1>
-            <button onClick={goBack}>Go Back</button>
->>>>>>> b52313201a88c0084679676a5a4a9b6765985b7b
+            <h1 style={inlineStyle}>This is the Contact page</h1>            <button>Go Back</button>
         </div>
     );
 }
