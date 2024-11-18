ac8ea371230395858b0f971ab77b73f12b3cfdf7
deanabii <117475244+deanabii@users.noreply.github.com>
2024-11-18 16:44:32 +0700

    project

--- react-client/src/components/Project.tsx
+++ react-client/src/components/Project.tsx
@@ -1,21 +1,3 @@
-// import { useNavigate } from "react-router-dom";
-
-// function Project(){
-//     const navigate = useNavigate();
-//     const goBack = () => {
-//         // navigate(-1); // Navigate to the previous page using the "goBack" method from react-router-dom
-//         navigate("/");
-//     };
-//     return(
-//         <div>
-//             <h1>This is the Project</h1>
-//             <button onClick={goBack}>Go Back</button>
-//         </div>
-//     );
-// }
-
-// export default Project;
-
 import { Card, CardBody,Divider, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import axios from "axios";
 import { useEffect, useState } from "react";
